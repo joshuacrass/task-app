c7375e5226aa7bb6a64522babeb8c7f8896277ed
Joshua Crass <joshua.crass@gmail.com>
2023-01-31 06:50:30 -0800

    updated form to clear the input and reset focus

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,19 +1,21 @@
+import { forwardRef } from "react";
 import styled from "styled-components";
 
 const StyledInput = styled.input`
   font-size: 2rem;
 `;
 
-const Input = (props) => {
+const Input = forwardRef((props, ref) => {
   const { type, value, change } = props;
 
   return (
     <StyledInput
+      ref={ref}
       type={type}
       value={value}
       onChange={(e) => change(e.target.value)}
     />
   );
-};
+});
 
 export default Input;
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,7 +1,7 @@
 import Input from "./Input";
 import Button from "./Button";
 
-import { useState, useContext } from "react";
+import { useState, useContext, useRef } from "react";
 import { TasksDispatchContext } from "../context/taskContext";
 
 import styled from "styled-components";
@@ -15,21 +15,33 @@ const StyledTaskForm = styled.div`
 const TaskForm = () => {
   const [task, setTask] = useState("");
   const dispatch = useContext(TasksDispatchContext);
+  const inputRef = useRef(null);
 
+  // update state as use type in the input field
   function handleInputChange(value) {
     setTask(value);
   }
 
+  // add the new task
   function handleButtonClick(action) {
     dispatch({
       type: action,
       name: task,
     });
+    // clear the form by updating state
+    setTask("");
+    // reset focus to the input
+    inputRef.current.focus();
   }
 
   return (
     <StyledTaskForm>
-      <Input type="text" value={task} change={handleInputChange} />
+      <Input
+        ref={inputRef}
+        type="text"
+        value={task}
+        change={handleInputChange}
+      />
       <Button text="Add" click={handleButtonClick} action="add" />
     </StyledTaskForm>
   );
